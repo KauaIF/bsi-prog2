ece9edce6cc87ea9d278e8f28c6a98cc0db3acab
KauaIF <kaua271203@gmail.com>
2024-11-27 09:17:09 -0300

    agenda v2

--- atividades/dicionarioLista2/ativi7telefones/main.py
+++ atividades/dicionarioLista2/ativi7telefones/main.py
@@ -1,3 +1,4 @@
+import os
 #função que abre o arquivo e registra os dados
 def carregar(n_arq:str)->dict:
     #dicionario a ser preenchido 'D'
@@ -11,25 +12,41 @@ def carregar(n_arq:str)->dict:
         #pelo "\t" (que é o caracter TAB)
         lst = linha.strip().split(",")
         #lst representa uma linha da minha tabela no formato de uma lista
-        D[lst[1]]=lst[0]
+        D[lst[0]]=lst[1:]
         #leia uma nova linha e começe o ciclo de novo
         linha = arq.readline()
     #retorna um dicionário com os números e quantas vezes apareceram
     return D
-def inserir(arq,nome,tel):
-    pass
+def inserir(arq,tel,nome,email):
+    base = carregar(arq)
+    if bool(base.get(tel,False)):
+        ans = input("O contato já existe deseja substituir? [s/N]")
+        if ans=='s':
+            base[tel] = [nome,email]
+            a = open(arq,"wt")
+            for i in base.keys():
+                escreva = i
+                for dado in base[i]:
+                    escreva += f",{dado}"
+                a.write(escreva)
+        else:
+            print('ação cancelada')
+    else:
+        print('aaa')
+    input()
 
 def main():
-    resultado = carregar("telefones.txt")
-    print(
-            """Selecione\n1)para inserir dados\n2)para ler dados\n3)para deletar dados"""
-        )
-    entrada = input("").lower()
+    os.system("cls")
+    arquivo_ini = "telefones.txt"
+    resultado = carregar(arquivo_ini)
+    entrada = input("Selecione\n1)para inserir dados\n2)para ler dados\n3)para deletar dados\n").lower()
     while entrada != '':
-        if entrada == 1:
-            inserir("telefones.txt",input("nome: "),input("tel: "))
-        print(
-            """Selecione\n1)para inserir dados\n2)para ler dados\n3)para deletar dados"""
-        )
-        entrada = input("")
+        os.system("cls")
+        if entrada == '1':
+            inserir(arquivo_ini,input("tel: "),input("nome: "),input("email: "))
+        if entrada == '2':
+            print(carregar(arquivo_ini))
+            input()
+        os.system("cls")
+        entrada = input("Selecione\n1)para inserir dados\n2)para ler dados\n3)para deletar dados\n").lower()
 if __name__=="__main__":main()
